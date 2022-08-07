cceb45e8f1c49a7d826134a16e75751b49e904af
Rivet Lorddom <er@Rivets-MacBook-Pro.local>
2022-08-07 17:18:12 +0900

    implemented save food

--- what_to_eat.py
+++ what_to_eat.py
@@ -3,20 +3,24 @@ from os.path import exists
 
 
 def main():
-    print("Welcome! I am here to help you decide what to eat!")
+    print("\n    Welcome! I am here to help you decide what to eat!")
+    print("   ====================================================\n")
     choice = get_choice()
 
     if choice == "eat":
-        get_food()
+        food_to_eat = get_food()
+        print(f"\n    Today you should eat: {food_to_eat}")
+
 
     if choice == "add":
-        new_food = input("Name of the food that you want to save: ")
+        new_food = input("\nName of the dish that you want to save: ").strip().capitalize()
         save_food(new_food)
 
     if choice == "del":
-        ...
+        print("This option is not implemented yet")
 
-    print(random.randint(1, 100))
+    print("\nThank you, see you again!")
+    print("=========================")
 
 
 def get_choice():
@@ -24,7 +28,7 @@ def get_choice():
     Asks the user for input in a loop, returns a choice string only if the input is valid
     """
     while True:
-        choice = input("Would you like to eat something or add a new item to you list of your favorite foods? (eat/add): ")
+        choice = input("Would you like to eat something or add a new item to the list of your favorite dishes? (eat/add): ")
         if choice in ["eat", "add", "del"]:
             return choice
 
@@ -35,14 +39,16 @@ def get_food():
     if not exists("foodlist.txt"):
         sys.exit("No favorite food registered yet. Try again.")
 
-    # open file
+    # open file and return one random dish name
     with open("foodlist.txt") as file:
-        ...
+        lines = file.readlines()
+        return random.choice(lines).strip()
 
 
 def save_food(food_name):
+
     with open("foodlist.txt", "a") as file:
-        file.write(food_name)
+        file.write(f"{food_name}\n")
     
 
 
