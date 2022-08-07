3ff359c6bb82c364c3b50fcab894d75febdc1990
Rivet Lorddom <er@Rivets-MacBook-Pro.local>
2022-08-07 16:43:05 +0900

    added get_choice function

--- what_to_eat.py
+++ what_to_eat.py
@@ -2,10 +2,37 @@ import random
 
 
 def main():
-    print("Are you hungry?")
+    print("Welcome! I am here to help you decide what to eat!")
+    choice = get_choice()
+
+    if choice == "eat":
+        ...
+
+    if choice == "add":
+        ...
+
+    if choice == "del":
+        ...
+
     print(random.randint(1, 100))
 
 
+def get_choice():
+    """
+    Asks the user for input in a loop, returns a choice string only if the input is valid
+    """
+    while True:
+        choice = input("Would you like to eat something or add a new item to you list of your favorite foods? (eat/add): ")
+        if choice in ["eat", "add", "del"]:
+            return choice
+
+
+def get_food():
+    ...
+
+
+def three():
+    ...
 
 
 if __name__ == "__main__":
